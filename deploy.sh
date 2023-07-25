618a5055ab9a1e335d5f1a756823312e1268ae5c
ava-vs <139607751+ava-vs@users.noreply.github.com>
2023-07-25 10:40:21 +0400

    Add frontend (#2)
    * add simple front
    * fix README
    
    ---------
    
    Co-authored-by: Ilia <justloreland@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+dfx stop
+set -e
+# trap 'dfx stop' EXIT
+trap 'echo "DONE"' EXIT
+
+dfx start --background --clean
+
+dfx canister create rep
+dfx canister create ver
+dfx canister create nft_container
+dfx canister create ava_poc_full_frontend
+
+dfx build
+
+dfx canister install rep
+dfx canister install ver
+dfx canister install ava_poc_full_frontend
+
+
+VerCanisterId=$(dfx canister id ver)
+
+dfx deploy --argument "(
+  principal\"$(dfx identity get-principal)\", 
+  record {
+    logo = record {
+      logo_type = \"image/png\";
+      data = \"1\";
+    };
+    name = \"Ava dNFT\";
+    symbol = \"AVA1\";
+    maxLimit = 1;
+  }
+)" nft_container
+
+echo "Creating dNFT"
+
+dfx canister call nft_container mintDip721 \
+"(
+  principal\"$(dfx identity get-principal)\", 
+  vec { 
+    record {
+      purpose = variant{Rendered};
+      data = blob\"Start\";
+      key_val_data = vec {
+        record { key = \"description\"; val = variant{TextContent=\"The aVa dNFT metadata\"}; };
+        record { key = \"contentType\"; val = variant{TextContent=\"text/plain\"}; };
+        record { key = \"locationType\"; val = variant{Nat8Content=4:nat8}; };
+        record { key = \"reputationBalance\"; val = variant{LinkContent=\"$(dfx canister id ver).getBalance\"} };
+      }
+    }
+  }
+)"
+
+echo "dNFT has been created!"
+
+echo "Metadata: "
+
+dfx canister call nft_container getMetadataDip721 "0"
+
+echo " "
+
+echo "Call Verification canister for current principal"
+
+dfx canister call ver init $(dfx identity get-principal)
+
+echo "Call Reputation canister and increase reputation balance for current user"
+
+dfx canister call rep incrementBalance '("'$(dfx identity get-principal)'", 1)'
+
+echo "dNFT metadata will track reputation balance changes!"
+
+
---
+++ src/ava_poc_full_frontend/src/index.js
@@ -0,0 +1,88 @@
+import { ver } from "../../declarations/ver";
+import { rep } from "../../declarations/rep";
+
+//get reputation balance
+
+document.getElementById("getBalanceButton").addEventListener("click", async (e) => {
+  e.preventDefault();
+  const button = e.target;
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+  let res_balance;
+  try {
+     res_balance = await ver.getBalance(name);
+    document.getElementById("balanceResult").innerText = "Current Balance (canister Ver): " + res_balance;
+} catch (error) {
+    console.error("Error getting balance:", error);
+    document.getElementById("balanceResult").innerText = "Error getting balance!";
+}
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("balanceResult").innerText = res_balance;
+  
+});
+
+// all users
+
+document.getElementById("getAllUsers").addEventListener("click", async (e) => {
+  e.preventDefault();
+  const button = e.target;
+
+  button.setAttribute("disabled", true);
+  let all_users;
+  try {
+    all_users = await rep.getUsers();
+
+    const tableBody = document.getElementById("usersTableBody");
+    tableBody.innerHTML = ""; 
+
+  all_users.forEach(user => {
+    // Создаем новую строку и ячейки для Principal и Int
+    const row = document.createElement("tr");
+    const principalCell = document.createElement("td");
+    const intCell = document.createElement("td");
+
+    // Заполняем ячейки данными из массива
+    principalCell.textContent = user[0] || "ao6hk-x5zgr-aa6y2-zq5ei-meewq-doeim-hwbws-zzxql-rjtcc-hmabt-xqe";
+    intCell.textContent = user[1] || "unknown";
+
+    // Добавляем ячейки в строку
+    row.appendChild(principalCell);
+    row.appendChild(intCell);
+
+    // Добавляем строку в tbody
+    tableBody.appendChild(row);
+});
+  } catch (error) {
+    console.error("Error getting users:", error);
+    document.getElementById("errorOutput").innerText = "Error getting users!"; 
+  }
+
+  button.removeAttribute("disabled");
+});
+
+
+// increase reputation
+
+document.getElementById("increaseRepoButton").addEventListener("click", async (e) => {
+  e.preventDefault();
+  const button = e.target;
+
+  const user = document.getElementById("user").value.toString();
+  button.setAttribute("disabled", true);
+
+    // Interact with rep actor, calling the increase method
+    try {
+      await rep.incrementBalance(user, 1);
+      document.getElementById("increaseResult").innerText = "Reputation in canister Rep increased!";
+  } catch (error) {
+      console.error("Error getting balance:", error);
+      document.getElementById("increaseResult").innerText = "Error getting balance!";
+  }  
+    
+    button.removeAttribute("disabled");   
+
+});
